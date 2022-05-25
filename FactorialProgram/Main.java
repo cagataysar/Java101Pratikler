1cc0bd1e7da1d458bf4be1387cca8ef4b7d60f1a
Çağatay Sarıoğlan <73501439+cagataysar@users.noreply.github.com>
2022-05-25 20:03:29 +0300

    Add files via upload

---
+++ FactorialProgram/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Bir sayı girin : ");
+        int n = input.nextInt();
+        int total = 1;
+
+        for (int i = 1; i <= n; i++){
+            total = total * i;
+        }
+        System.out.println(n + ". Faktoriyel : " + total);
+
+    }
+}
